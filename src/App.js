85b44054de912d0317fb15ecb7636b3ff360e5ff
rehanGH <rehangh8924@gmail.com>
2023-01-08 20:49:43 -0700

    added styling using bootstrap

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import aes128 from './aes128';
 import aes192 from './aes192';
 import aes256 from './aes256';
 
-
 const App = () => {
   const [mode, setMode] = useState('aes-128');
   const [message, setMessage] = useState('');
@@ -40,7 +39,7 @@ const App = () => {
       <h1>AES Encrypt/Decrypt</h1>
       <div className="form-group">
         <label htmlFor="mode-select">Select AES Mode:</label>
-        <select id="mode-select" value={mode} onChange={e => setMode(e.target.value)}>
+        <select id="mode-select" value={mode} onChange={e => setMode(e.target.value)} className="form-control">
           <option value="aes-128">AES-128</option>
           <option value="aes-192">AES-192</option>
           <option value="aes-256">AES-256</option>
@@ -48,28 +47,28 @@ const App = () => {
       </div>
       <div className="form-group">
         <label htmlFor="message-input">Message:</label>
-        <textarea id="message-input" value={message} onChange={e => setMessage(e.target.value)} />
+        <textarea id="message-input" value={message} onChange={e => setMessage(e.target.value)} className="form-control" />
       </div>
       <div className="form-group">
         <label htmlFor="password-input">Password:</label>
-        <input type="password" id="password-input" value={password} onChange={e => setPassword(e.target.value)} />
+        <input type="password" id="password-input" value={password} onChange={e => setPassword(e.target.value)} className="form-control" />
       </div>
-      <button type="button" onClick={handleEncrypt}>
-        Encrypt
+      <button type="button" onClick={handleEncrypt} className="btn btn-primary">
+        Encrypt          
       </button>
-      <button type="button" onClick={handleDecrypt}>
+      <button type="button" onClick={handleDecrypt} className="btn btn-primary">
         Decrypt
       </button>
       <div className="form-group">
-      <label htmlFor="encrypted-message">Encrypted Message:</label>
-        <textarea id="encrypted-message" value={encryptedMessage} onChange={e => setEncryptedMessage(e.target.value)} />
+        <label htmlFor="encrypted-message">Encrypted Message:</label>
+        <textarea id="encrypted-message" value={encryptedMessage} onChange={e => setEncryptedMessage(e.target.value)} className="form-control" />
       </div>
-        <div className="form-group">
-            <label htmlFor="decrypted-message">Decrypted Message:</label>
-        <textarea id="decrypted-message" value={decryptedMessage} onChange={e => setDecryptedMessage(e.target.value)} />
+      <div className="form-group">
+        <label htmlFor="decrypted-message">Decrypted Message:</label>
+        <textarea id="decrypted-message" value={decryptedMessage} onChange={e => setDecryptedMessage(e.target.value)} className="form-control" />
       </div>
     </div>
-    );
+  );
 };
 
 export default App;
